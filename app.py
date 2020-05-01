3e052c004cbccd65dc9e27a37cf07eaccb740d7d
Oleksiy Statsenko <alexey.statsenko@gmail.com>
2020-05-01 09:29:26 +0000

    Small change

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ APP = Flask(__name__)
 
 @APP.route('/')
 def hello_world():
-    return render_template('pages/index.html', title="The Lazy Padwan")
+    return render_template('pages/index.html', title="The Lazy Padwan and his lost Son")
 
 if __name__ == '__main__':
     APP.run(host=os.environ.get('HOSTNAME'),
